4485aaf71b4719a4adea18043c5be293ae622d90
Anastasiya <iinokayo@mail.ru>
2023-02-22 14:26:07 +0300

    Finished

--- scripts/chart.js
+++ scripts/chart.js
@@ -1,14 +1,28 @@
-// init chart
+// init chart in existing element
 const myChart = echarts.init(document.getElementById('main'));
 
 // options for chart
 const option = {
   title: {
+    left: '30px',
     text: 'Проекты в программах и вне программ',
+    textStyle: {
+      fontSize: 16,
+      fontWeight: 600,
+      color: '#002033',
+    },
     subtext:
       'Сумма и процентное соотношение проектов, находящихся в программах и вне программ',
+    subtextStyle: {
+      fontSize: 14,
+      fontWeight: 400,
+      color: '#00203399',
+    },
   },
-  tooltip: {},
+  tooltip: {
+    trigger: 'axis',
+    formatter: (params) => createTooltip(params),
+  },
   legend: {
     data: Array.from(legends).sort(),
     top: 'bottom',
@@ -17,8 +31,11 @@ const option = {
   xAxis: {
     data: Array.from(periods),
   },
-  yAxis: {},
+  yAxis: {
+    type: 'value',
+  },
   series: [...chartSeries, ...labels],
 };
 
+// add options in chart
 myChart.setOption(option);
